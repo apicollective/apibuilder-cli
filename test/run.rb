57b30522151a40598bf5345722367a349ff8e3de
Michael Bryzek <michael@gilt.com>
2015-02-20 18:17:37 -0600

    Layout test structure

---
+++ test/run.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+load File.join(File.dirname(__FILE__), '..', 'lib', 'apidoc-cli.rb')
+
+def run(command)
+  puts command
+  system(command)
+end
+
+gem_home = File.join(File.dirname(__FILE__), '../gems')
+rspec = File.join(gem_home, 'bin/rspec')
+if !File.exists?(rspec)
+  run("export GEM_HOME=%s && gem install rspec --version 3.2.0 --no-rdoc --no-ri --install-dir %s" % [gem_home, gem_home])
+end
+
+files = `find specs -type f -name "*spec.rb"`.strip.split
+
+result = output = nil
+
+tmp = "/tmp/apidoc-cli.tmp"
+result = run("export GEM_HOME=%s && %s %s > %s" % [gem_home, rspec, files.join(" "), tmp])
+output = IO.read(tmp)
+puts output
+
+if result
+  exit(0)
+else
+  puts ""
+  puts "To rerun particular scripts, use rspec at:"
+  puts "export GEM_HOME=%s && %s" % [gem_home, rspec]
+  exit(1)
+end
---
+++ test/spec/lib/preconditions_spec.rb
@@ -0,0 +1,59 @@
+load File.join(File.dirname(__FILE__), '../../init.rb')
+
+RSpec.describe ApidocCli::Preconditions do
+
+  # it "ApidocCli::Preconditions.check_argument" do
+  #   ApidocCli::Preconditions.check_argument(true, "Noop")
+  #   expect_exception {
+  #     ApidocCli::Preconditions.check_argument(false, "Should be this")
+  #   }
+  # end
+
+  # it "ApidocCli::Preconditions.check_state" do
+  #   ApidocCli::Preconditions.check_argument(1 > 0, "Noop")
+  #   expect_exception {
+  #     ApidocCli::Preconditions.check_argument(1 < 0, "Should be this")
+  #   }
+  # end
+
+  # it "ApidocCli::Preconditions.check_not_null" do
+  #   ApidocCli::Preconditions.check_argument("", "Noop")
+  #   expect_exception {
+  #     ApidocCli::Preconditions.check_argument(nil, "Should be this")
+  #   }
+  # end
+
+  # it "ApidocCli::Preconditions.check_not_blank" do
+  #   expect_exception {
+  #     ApidocCli::Preconditions.check_not_blank(nil, "Should be this")
+  #   }
+  #   expect {
+  #     ApidocCli::Preconditions.check_not_blank("")
+  #   }.to raise_error(RuntimeError)
+  # end
+
+  # it "ApidocCli::Preconditions.assert_empty_opts" do
+  #   ApidocCli::Preconditions.assert_empty_opts({})
+  #   expect {
+  #     ApidocCli::Preconditions.assert_empty_opts({ "a" => "b" })
+  #   }.to raise_error(RuntimeError)
+  # end
+
+  # it "ApidocCli::Preconditions.assert_class" do
+  #   ApidocCli::Preconditions.assert_class("string", String)
+  #   ApidocCli::Preconditions.assert_class(1, Integer)
+  #   expect {
+  #     ApidocCli::Preconditions.assert_class(1, String)
+  #   }.to raise_error(RuntimeError)
+  # end
+
+  it "ApidocCli::Preconditions.assert_class_or_nil" do
+    ApidocCli::Preconditions.assert_class_or_nil("string", String)
+    ApidocCli::Preconditions.assert_class_or_nil(nil, String)
+    ApidocCli::Preconditions.assert_class_or_nil(1, Integer)
+    expect {
+      ApidocCli::Preconditions.assert_class_or_nil(1, String)
+    }.to raise_error(RuntimeError)
+  end
+
+end
