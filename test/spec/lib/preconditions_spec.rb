87fb2bbe195a584099421598058e8cb7f8e05aac
Michael Bryzek <michael@gilt.com>
2015-02-21 08:38:55 -0500

    First working specs

--- test/spec/lib/preconditions_spec.rb
+++ test/spec/lib/preconditions_spec.rb
@@ -2,50 +2,51 @@ load File.join(File.dirname(__FILE__), '../../init.rb')
 
 RSpec.describe ApidocCli::Preconditions do
 
-  # it "ApidocCli::Preconditions.check_argument" do
-  #   ApidocCli::Preconditions.check_argument(true, "Noop")
-  #   expect_exception {
-  #     ApidocCli::Preconditions.check_argument(false, "Should be this")
-  #   }
-  # end
+  it "ApidocCli::Preconditions.check_argument" do
+    ApidocCli::Preconditions.check_argument(true, "Noop")
+    expect {
+      ApidocCli::Preconditions.check_argument(false, "Should be this")
+    }.to raise_error(RuntimeError)
+  end
 
-  # it "ApidocCli::Preconditions.check_state" do
-  #   ApidocCli::Preconditions.check_argument(1 > 0, "Noop")
-  #   expect_exception {
-  #     ApidocCli::Preconditions.check_argument(1 < 0, "Should be this")
-  #   }
-  # end
+  it "ApidocCli::Preconditions.check_state" do
+    ApidocCli::Preconditions.check_argument(1 > 0, "Noop")
+    expect {
+      ApidocCli::Preconditions.check_argument(1 < 0, "Should be this")
+    }.to raise_error(RuntimeError)
+  end
 
-  # it "ApidocCli::Preconditions.check_not_null" do
-  #   ApidocCli::Preconditions.check_argument("", "Noop")
-  #   expect_exception {
-  #     ApidocCli::Preconditions.check_argument(nil, "Should be this")
-  #   }
-  # end
+  it "ApidocCli::Preconditions.check_not_null" do
+    ApidocCli::Preconditions.check_argument("", "Noop")
+    expect {
+      ApidocCli::Preconditions.check_argument(nil, "Should be this")
+    }.to raise_error(RuntimeError)
+  end
 
-  # it "ApidocCli::Preconditions.check_not_blank" do
-  #   expect_exception {
-  #     ApidocCli::Preconditions.check_not_blank(nil, "Should be this")
-  #   }
-  #   expect {
-  #     ApidocCli::Preconditions.check_not_blank("")
-  #   }.to raise_error(RuntimeError)
-  # end
+  it "ApidocCli::Preconditions.check_not_blank" do
+    expect {
+      ApidocCli::Preconditions.check_not_blank(nil, "Should be this")
+    }.to raise_error(RuntimeError)
 
-  # it "ApidocCli::Preconditions.assert_empty_opts" do
-  #   ApidocCli::Preconditions.assert_empty_opts({})
-  #   expect {
-  #     ApidocCli::Preconditions.assert_empty_opts({ "a" => "b" })
-  #   }.to raise_error(RuntimeError)
-  # end
+    expect {
+      ApidocCli::Preconditions.check_not_blank("")
+    }.to raise_error(RuntimeError)
+  end
 
-  # it "ApidocCli::Preconditions.assert_class" do
-  #   ApidocCli::Preconditions.assert_class("string", String)
-  #   ApidocCli::Preconditions.assert_class(1, Integer)
-  #   expect {
-  #     ApidocCli::Preconditions.assert_class(1, String)
-  #   }.to raise_error(RuntimeError)
-  # end
+  it "ApidocCli::Preconditions.assert_empty_opts" do
+    ApidocCli::Preconditions.assert_empty_opts({})
+    expect {
+      ApidocCli::Preconditions.assert_empty_opts({ "a" => "b" })
+    }.to raise_error(RuntimeError)
+  end
+
+  it "ApidocCli::Preconditions.assert_class" do
+    ApidocCli::Preconditions.assert_class("string", String)
+    ApidocCli::Preconditions.assert_class(1, Integer)
+    expect {
+      ApidocCli::Preconditions.assert_class(1, String)
+    }.to raise_error(RuntimeError)
+  end
 
   it "ApidocCli::Preconditions.assert_class_or_nil" do
     ApidocCli::Preconditions.assert_class_or_nil("string", String)
