5ac1c21b992bbb36e7cbe5137b3647e12a70bae0
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-02 16:32:27 -0400

    autocommit: Update version to 0.0.14

--- lib/apidoc-cli/version.rb
+++ lib/apidoc-cli/version.rb
@@ -2,7 +2,7 @@ module ApidocCli
 
   module Version
 
-    VERSION = '0.0.13' # Automatically updated by util/create-release.rb
+    VERSION = '0.0.14' # Automatically updated by util/create-release.rb
 
     # Writes contents to a temp file, returning the path
     def Version.current
