b10b3f8158ffffd795837eb5f1c4455ced6c5dd3
Michael Bryzek <mbryzek@alum.mit.edu>
2017-08-28 17:23:14 -0400

    autocommit: Update version to 0.1.5

--- lib/apibuilder-cli/version.rb
+++ lib/apibuilder-cli/version.rb
@@ -2,7 +2,7 @@ module ApibuilderCli
 
   module Version
 
-    VERSION = '0.1.4' # Automatically updated by util/create-release.rb
+    VERSION = '0.1.5' # Automatically updated by util/create-release.rb
 
     # Writes contents to a temp file, returning the path
     def Version.current
