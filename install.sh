f1cdf4ab7416441bc2416f84d5c2be11f712f6d6
Tom Gamble <thomasgamble2@gmail.com>
2017-08-29 16:59:15 -0400

    updates to support homebrew install (#22)

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# install script from https://github.com/sstephenson/API_BUILDER
+set -e
+
+resolve_link() {
+  $(type -p greadlink readlink | head -1) "$1"
+}
+
+abs_dirname() {
+  local cwd="$(pwd)"
+  local path="$1"
+
+  while [ -n "$path" ]; do
+    cd "${path%/*}"
+    local name="${path##*/}"
+    path="$(resolve_link "$name" || true)"
+  done
+
+  pwd
+  cd "$cwd"
+}
+
+PREFIX="$1"
+if [ -z "$1" ]; then
+  { echo "usage: $0 <prefix>"
+    echo "  e.g. $0 /usr/local"
+  } >&2
+  exit 1
+fi
+
+API_BUILDER_ROOT="$(abs_dirname "$0")"
+mkdir -p "$PREFIX"/{bin,src}
+cp -R "$API_BUILDER_ROOT"/bin/* "$PREFIX"/bin
+cp -R "$API_BUILDER_ROOT"/src/* "$PREFIX"/src
+
+echo "Installed API_BUILDER_CLI to $PREFIX/bin/api-builder-cli"
