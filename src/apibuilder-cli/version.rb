e7914313addca33c4062eecf9dee71c7a2efbcbb
Michael Bryzek <mike@flow.io>
2018-04-09 10:55:51 -0400

    autocommit: Update version to 0.1.23

--- src/apibuilder-cli/version.rb
+++ src/apibuilder-cli/version.rb
@@ -2,7 +2,7 @@ module ApibuilderCli
 
   module Version
 
-    VERSION = '0.1.22' # Automatically updated by util/create-release.rb
+    VERSION = '0.1.23' # Automatically updated by util/create-release.rb
 
     # Writes contents to a temp file, returning the path
     def Version.current
