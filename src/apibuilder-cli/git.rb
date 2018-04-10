04bc1ab934ad9832c7fb8a3790fd356c00403434
Ryan Martin <rlmartin@gmail.com>
2018-04-09 21:55:47 -0400

    Remove unintentional console output from Git.safe_describe

--- src/apibuilder-cli/git.rb
+++ src/apibuilder-cli/git.rb
@@ -4,7 +4,7 @@ module ApibuilderCli
 
     def Git.safe_describe(commits_back = nil)
       head = commits_back.nil? ? "" : "HEAD~#{commits_back}"
-      system("git describe #{head} 2> /dev/null") ? `git describe #{head}`.strip : ""
+      system("git describe #{head} > /dev/null 2>&1") ? `git describe #{head}`.strip : ""
     end
 
   end
